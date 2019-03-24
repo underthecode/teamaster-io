31ef4249dcb010af691f9cbdcf2ce130cbbb1396
スティーブンさん <underyourcover@icloud.com>
2019-03-23 19:32:41 -0700

    init passport boiler plate

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000;
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'verified heroku auto deploy' });
-});
+const app = express();
+
+passport.use(new GoogleStrategy());
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
