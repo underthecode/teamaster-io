713679d1915354264d8b5b7517a4a31082b3c04f
スティーブンさん <underyourcover@icloud.com>
2019-03-25 17:49:06 -0700

    imported && invoked authRoutes in server/index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
