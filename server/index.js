5c043cac5d4f0e129a1f02483f5c8d5e12f1685b
スティーブンさん <underyourcover@icloud.com>
2019-03-25 17:34:43 -0700

    moved passport config to passport

--- server/index.js
+++ server/index.js
@@ -5,23 +5,6 @@ const keys = require('./config/keys');
 
 const app = express();
 
-// configures passport
-passport.use(
-  // this has internal code to identify 'google' as a string
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
