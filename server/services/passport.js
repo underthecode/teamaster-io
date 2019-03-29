12d2013c12105b39d0446bb82b3471bc1668b10d
スティーブンさん <underyourcover@icloud.com>
2019-03-28 22:18:06 -0700

    init 'profile.id' save to db

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 // configures passport
 passport.use(
@@ -12,9 +15,7 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
+      new User({ googleId: profile.id }).save();
     }
   )
 );
