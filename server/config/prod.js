a33350f8523c02ac7d6b632ca0195f96a9ea6c7e
スティーブンさん <underyourcover@icloud.com>
2019-03-29 20:31:53 -0700

    fixed spelling/comment error

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,5 @@
-// prod.js - DONT COMMIT THIS
-odule.exports = {
+// prod.js - contains prod keys
+module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
