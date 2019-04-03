ce5cb80e90d0c43f3609c58caca93e531504586e
スティーブンさん <underyourcover@icloud.com>
2019-04-02 17:32:49 -0700

    commented out devServer property

--- webpack.config.js
+++ webpack.config.js
@@ -20,15 +20,15 @@ module.exports = {
   },
   resolve: { extensions: ['*', '.js', '.jsx'] },
   output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: './client/dist/'
+    path: path.resolve(__dirname, 'client/dist/'),
+    publicPath: './client/dist/',
     filename: 'bundle.js'
   },
-  devServer: {
-    contentBase: path.join(__dirname, "client/"),
-    port: 5000,
-    publicPath: "http://localhost:5000/dist",
-    hotOnly: true
-  },
+  // devServer: {
+  //   contentBase: path.join(__dirname, 'client/dist/'),
+  //   port: 5000,
+  //   publicPath: 'http://localhost:5000/dist/',
+  //   hotOnly: true
+  // },
   plugins: [new webpack.HotModuleReplacementPlugin()]
 };
