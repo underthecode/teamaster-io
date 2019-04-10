14a8a3d1179be2d28dac818a7e7f76d4882cb20c
スティーブンさん <underyourcover@icloud.com>
2019-04-09 18:25:04 -0700

    imported react-dom helpers, init dummy components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>app renders!</h1>
-        <a href="/auth/google">sign in with Google</a>
+        <h1>App renders!</h1>
+        <a href="/auth/google">Sign In with Google</a>
       </div>
     );
   }
