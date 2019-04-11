4f3aad59926fb532dab88aeaf84606f5ae18cec1
スティーブンさん <underyourcover@icloud.com>
2019-04-11 03:05:56 -0700

    disabled react-hot-loader for now...

--- webpack.config.js
+++ webpack.config.js
@@ -33,12 +33,12 @@ module.exports = {
     path: path.resolve(__dirname, 'client/dist/'),
     publicPath: './client/dist/',
     filename: 'bundle.js'
-  },
+  }
   // devServer: {
   //   contentBase: path.join(__dirname, 'client/dist/'),
   //   port: 5000,
   //   publicPath: 'http://localhost:5000/dist/',
   //   hotOnly: true
   // },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  // plugins: [new webpack.HotModuleReplacementPlugin()]
 };
