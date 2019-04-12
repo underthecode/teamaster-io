a289a0ab5313cad41a4bac98f8e145915b9ef89c
スティーブンさん <underyourcover@icloud.com>
2019-04-12 05:16:58 -0700

    confiugred fetchUser to use thunk

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
+// treat as async, return dispatch upon successful get
 const fetchUser = () => {
-  axios.get('./api/current_user');
+  return function(dispatch) {
+    axios
+      .get('./api/current_user')
+      .then(res => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
