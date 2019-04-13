9e3640d9853b27b68354ee416df2f62cd0cf2f83
スティーブンさん <underyourcover@icloud.com>
2019-04-12 17:25:31 -0700

    additional arrow function refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 
 // treat as async, return dispatch upon successful get
-export const fetchUser = () =>
-  function(dispatch) {
-    axios
-      .get('./api/current_user')
-      .then(res => dispatch({ type: FETCH_USER, payload: res }));
-  };
+export const fetchUser = () => dispatch => {
+  axios
+    .get('./api/current_user')
+    .then(res => dispatch({ type: FETCH_USER, payload: res }));
+};
