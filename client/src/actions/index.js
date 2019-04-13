7da2c349116505f6ae17567b661bb1380e7b3e89
スティーブンさん <underyourcover@icloud.com>
2019-04-13 01:38:11 -0700

    configured to return only 'data' prop

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from './types';
 // treat as async, return dispatch upon successful get
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('./api/current_user');
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
