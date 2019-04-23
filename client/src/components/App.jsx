31400c7f272e9d59835910f89d2e8056db054d7c
スティーブンさん <underyourcover@icloud.com>
2019-04-22 18:02:40 -0700

    init Landing component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing.jsx';
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
-const Landing = () => <h2>Landing</h2>;
 
 class App extends React.Component {
   // lifecycle to check if current user is signed in or not
