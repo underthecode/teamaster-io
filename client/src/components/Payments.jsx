edf77ed71cceb71be8b4e841f938abcf225c07e6
スティーブンさん <underyourcover@icloud.com>
2019-04-23 14:59:07 -0700

    styled Payments form and button

--- client/src/components/Payments.jsx
+++ client/src/components/Payments.jsx
@@ -5,10 +5,14 @@ class Payments extends React.Component {
   render() {
     return (
       <StripeCheckout
+        name="teamster-io"
+        description="$5 for 5 Survey Credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.STRIPE_KEY}
-      />
+      >
+        <button className="btn">Add Credits</button>
+      </StripeCheckout>
     );
   }
 }
