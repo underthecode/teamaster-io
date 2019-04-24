9e4d31908dace994c2b2cbdfea943e5da4edcb9b
スティーブンさん <underyourcover@icloud.com>
2019-04-23 17:57:08 -0700

    configured handleToken action for Stripe

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,3 +6,9 @@ export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/current_user');
   dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// return same dispatcha fter getting token back from Stripe API
+export const handleToken = token => async dispatch => {
+  const res = await axios.post('/api/stripe', token);
+  dispatch({ type: FETCH_USER, payload: res.data });
+};
