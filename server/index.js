1493e0b0a72eb95f00985da05f1b13d32e3067b8
スティーブンさん <underyourcover@icloud.com>
2019-05-07 13:36:05 -0700

    init Survey model

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
