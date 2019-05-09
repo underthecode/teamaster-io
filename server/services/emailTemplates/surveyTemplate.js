c6bd19f365c480b86ba4b2c181d7e215902dbdff
スティーブンさん <underyourcover@icloud.com>
2019-05-09 03:25:23 -0700

    styled email template minimally

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,3 +1,18 @@
 module.exports = survey => {
-  return `<div>${survey.body}/</div>`;
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3>I'd like to hear about your experience!</h3>
+          <p>Please answer the following question:</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="http://localhost:5000">Yes</a>
+          </div>
+          <div>
+            <a href="http://localhost:5000">No</a>
+          </div>
+        </div>
+      </body>
+    </html>`;
 };
