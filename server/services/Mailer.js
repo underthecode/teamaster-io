66e5f97ec1c63a264da1dee1b9a53d09e70c712a
スティーブンさん <underyourcover@icloud.com>
2019-05-08 17:23:31 -0700

    configured addRecipients method, finished setup

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -29,6 +29,14 @@ class Mailer extends helper.Mail {
     trackingSettings.setClickTracking(clickTracking);
     this.addTrackingSettings(trackingSettings);
   }
+
+  addRecipients() {
+    const personalize = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalize.addTo(recipient);
+    });
+    this.addPersonalization(personalize);
+  }
 }
 
 module.exports = Mailer;
