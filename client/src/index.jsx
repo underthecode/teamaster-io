2fa7a44638db268b8472706605d913a9339d6bf2
スティーブンさん <underyourcover@icloud.com>
2019-05-08 18:08:18 -0700

    successfully sends emails

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -11,6 +11,10 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// test email template logic
+import axios from 'axios';
+window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
