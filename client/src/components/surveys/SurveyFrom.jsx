8639b8e99576489346f0a35f1ec876fc724e4ed3
スティーブンさん <underyourcover@icloud.com>
2019-05-10 05:41:09 -0700

    created renderFields method for SoC

--- client/src/components/surveys/SurveyFrom.jsx
+++ client/src/components/surveys/SurveyFrom.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
 
 // SurveyForm renders a form for user to add input
 class SurveyForm extends React.Component {
+  renderFields() {
+    return (
+      <div>
+        <Field type="text" name="title" component={SurveyField} />
+      </div>
+    );
+  }
+
   render() {
     return (
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-          <Field type="text" name="surveyTitle" component="input" />
+          {this.renderFields()}
           <button type="submit">Submit</button>
         </form>
       </div>
