d495238bebe30146a6a74a6f8570ec4d288cdb3c
スティーブンさん <underyourcover@icloud.com>
2019-05-09 17:34:20 -0700

    init add icon && added header tag

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <div className="fixed-action-btn">
+        <a className="btn-floating btn-large green">
+          <i className="material-icons">add</i>/>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
