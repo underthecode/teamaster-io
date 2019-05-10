1b4de64d8845421c38c3bc178a245bda2f4897f0
スティーブンさん <underyourcover@icloud.com>
2019-05-10 04:47:26 -0700

    imported and rendered SurveyForm

--- client/src/components/surveys/SurveyNew.jsx
+++ client/src/components/surveys/SurveyNew.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SurveyForm from './SurveyFrom';
 
 class SurveyNew extends React.Component {
   render() {
     return (
       <div>
-        <h2>SurveyNew</h2>
+        <SurveyForm />
       </div>
     );
   }
