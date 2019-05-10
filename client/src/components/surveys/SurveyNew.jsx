790a28f81e153143480818743c3554a05a7dbc55
スティーブンさん <underyourcover@icloud.com>
2019-05-10 04:09:17 -0700

    renamed filetype to .jsx

---
+++ client/src/components/surveys/SurveyNew.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SurveyNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>SurveyNew</h2>
+      </div>
+    );
+  }
+}
+
+export default SurveyNew;
