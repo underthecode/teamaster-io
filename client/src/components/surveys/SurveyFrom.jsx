88d2a46d13b8cfd920e3d9c8c3ad34a95569cb0d
スティーブンさん <underyourcover@icloud.com>
2019-05-10 05:23:26 -0700

    configured Field props && tested onSubmit

--- client/src/components/surveys/SurveyFrom.jsx
+++ client/src/components/surveys/SurveyFrom.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 // SurveyForm renders a form for user to add input
 class SurveyForm extends React.Component {
   render() {
     return (
       <div>
-        <h2>SurveyForm</h2>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          <Field type="text" name="surveyTitle" component="input" />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     );
   }
