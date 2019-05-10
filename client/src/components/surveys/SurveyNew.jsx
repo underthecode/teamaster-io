80c291b31188abeeb909a624cdc2652dd0491fb5
スティーブンさん <underyourcover@icloud.com>
2019-05-10 05:04:26 -0700

    added comment

--- client/src/components/surveys/SurveyNew.jsx
+++ client/src/components/surveys/SurveyNew.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SurveyForm from './SurveyFrom';
 
+// SurveyNew renders SurveyForm && SurveyFormReview
 class SurveyNew extends React.Component {
   render() {
     return (
