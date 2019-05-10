2071ad8f4e091adc480766efb7d1c61103ff10fd
スティーブンさん <underyourcover@icloud.com>
2019-05-10 04:11:35 -0700

    rendered SurveyNew component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,7 +6,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends React.Component {
   // lifecycle to check if current user is signed in or not
