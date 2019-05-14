faaf67e70d95b8c100a67e9ae06b866218878940
スティーブンさん <underyourcover@icloud.com>
2019-05-14 00:44:57 -0700

    init form styling

--- client/src/components/surveys/SurveyFrom.jsx
+++ client/src/components/surveys/SurveyFrom.jsx
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 
 const FIELDS = [
@@ -31,7 +32,13 @@ class SurveyForm extends React.Component {
       <div>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );
