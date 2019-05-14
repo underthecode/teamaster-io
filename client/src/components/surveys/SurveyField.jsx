2f793ab58878dc48d42c4c476b2dbd976e0e6562
スティーブンさん <underyourcover@icloud.com>
2019-05-14 02:40:49 -0700

    init logic to display validation errors

--- client/src/components/surveys/SurveyField.jsx
+++ client/src/components/surveys/SurveyField.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 // SurveyField has logic to render single label && text input
-export default ({ input, label }) => {
+export default ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
       <label>{label}</label>
       <input {...input} />
+      {touched && error}
     </div>
   );
 };
