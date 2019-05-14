ce79e3f0275554f52fc352600345bad243f846ff
スティーブンさん <underyourcover@icloud.com>
2019-05-14 03:16:22 -0700

    completed validateEmails function logic

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,3 +1,14 @@
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
 export default emails => {
-  const emailsArr = emails.split(',').map(email => email.trim());
+  const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim())
+    .filter(email => emailRegex.test(email) === false);
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+
+  return;
 };
