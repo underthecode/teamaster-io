eb445d3e9835dfd6e0d27a921ce9c32cb5485a24
スティーブンさん <underyourcover@icloud.com>
2019-05-14 22:01:15 -0700

    added logic to show SurveyForm || SurveyFormReview

--- client/src/components/surveys/SurveyNew.jsx
+++ client/src/components/surveys/SurveyNew.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import SurveyForm from './SurveyFrom';
+import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 // SurveyNew renders SurveyForm && SurveyFormReview
 class SurveyNew extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { showFormReview: false };
+  }
+
+  renderContent() {
+    if (this.state.showFormReview) {
+      return <SurveyFormReview />;
+    } else {
+      return (
+        <SurveyForm
+          onSurveySubmit={() => this.setState({ showFormReview: true })}
+        />
+      );
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <SurveyForm />
-      </div>
-    );
+    return <div>{this.renderContent()}</div>;
   }
 }
 
