c5efe7e23705b41c7c6135770c34d14c7989a137
スティーブンさん <underyourcover@icloud.com>
2019-05-16 01:08:14 -0700

    refactored to dynamically render reviewFields

--- client/src/components/surveys/SurveyFormReview.jsx
+++ client/src/components/surveys/SurveyFormReview.jsx
@@ -1,12 +1,23 @@
 // SurveyFormReview renders a view for users to reivew SurveyForm inputs
+import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import formFields from './formFields';
 
 const SurveyFormReview = props => {
+  const reivewFields = _.map(formFields, field => {
+    return (
+      <div key={field.name}>
+        <label>{field.label}</label>
+        <div>{props.formValues[field.name]}</div>
+      </div>
+    );
+  });
+
   return (
     <div>
       <h5>Please confirm your filled out information</h5>
-
+      {reivewFields}
       <button className="yellow darken-3 btn-flat" onClick={props.onBack}>
         Back
       </button>
