3e530cb1707ba8ecf5ffd56749698b9af418dfc2
スティーブンさん <underyourcover@icloud.com>
2019-05-15 22:25:58 -0700

    configured onBack prop for false state

--- client/src/components/surveys/SurveyFormReview.jsx
+++ client/src/components/surveys/SurveyFormReview.jsx
@@ -1,9 +1,13 @@
+// SurveyFormReview renders a view for users to reivew SurveyForm inputs
 import React from 'react';
 
-const SurveyFormReview = () => {
+const SurveyFormReview = props => {
   return (
     <div>
       <h5>Please confirm your filled out information</h5>
+      <button className="yellow darken-3 btn-flat" onClick={props.onBack}>
+        Back
+      </button>
     </div>
   );
 };
