6f5af2e1c4ecbdb756627dffffb6fd9269afd18f
スティーブンさん <underyourcover@icloud.com>
2019-05-16 20:59:27 -0700

    configured filter records w/o surveyId && choice

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -18,8 +18,17 @@ module.exports = app => {
     const events = _.map(req.body, event => {
       const pathname = new URL(event.url).pathname;
       const p = new Path('/api/surveys/:surveyId/:choice');
-      console.log(p.test(pathname));
+      const match = p.test(pathname);
+
+      if (match) {
+        return {
+          email: event.email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
     });
+    console.log(events);
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
