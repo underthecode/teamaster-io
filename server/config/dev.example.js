bac3cc66acec72bf3851e0e27f6d31bba0044b9b
スティーブンさん <underyourcover@icloud.com>
2020-08-03 00:55:54 -0700

    init examples

---
+++ server/config/dev.example.js
@@ -0,0 +1,10 @@
+module.exports = {
+  googleClientID: '12345',
+  googleClientSecret: '12345-n99Z',
+  mongoURI: 'mongodb+srv://admin:12345',
+  cookieKey: '12345',
+  stripePublishableKey: 'pk_test_12345',
+  stripeSecretKey: 'sk_test_12345',
+  sendGridKey: 'SG.12345',
+  redirectDomain: 'http://localhost:5000'
+};
